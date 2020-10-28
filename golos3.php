e1812eb5de15d6481bf7345edaa8089f9edc0ece
LekasNet <58228148+LekasNet@users.noreply.github.com>
2020-10-28 22:58:21 +0300

    Add files via upload

--- golos3.php
+++ golos3.php
@@ -2,12 +2,12 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Голосование с выводом в новом окне</title>
+<title>Голосование с выводом в iframe</title>
 </head>
 <body>
 
-<form action="vote.php" method="get" target='_blank'>
-<input type="Hidden" name="id" value="1">
+<form action="vote.php" method="get" target="hero">
+<input type="Hidden" name="id" value="3">
 <p><b>Оцените сайт:</b></p>
 <input type="Radio" name="vote" value="1" checked>Отлично<br>
 <input type="Radio" name="vote" value="2">Хорошо<br>
@@ -17,7 +17,12 @@
 <input type="Submit" value=" Голосовать" style="margin-top:10px;">
 </form>
 
-<p><a href='vote.php?id=1' target='_blank'><button>Результаты</button></a></p>
+<p><a href='vote.php?id=3' target="hero"><button>Результаты</button></a></p>
+
+<iframe name="hero"  src="vote.php?id=3" width="150" height="200" frameborder="0" style="border: 0px solid black" scrolling="auto">
+    Ваш браузер не поддерживает плавающие фреймы!
+</iframe>
+<p>Lorem ipsum</p>
 
 </body>
 </html>
